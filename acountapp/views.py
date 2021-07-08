d0552153c0f9c9a1ee42ba7ea47aee3111fdac78
sara1060 <widejks96@gmail.com>
2021-07-08 16:12:26 +0900

    7/7 1ban class commit_0

--- acountapp/views.py
+++ acountapp/views.py
@@ -2,11 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from acountapp.models import BonBon
+
 
 def hello_world(request):
     if request.method=='POST':
+
+           temp = request.POST.get('hello_world_input')
+
+           new_bonbon = BonBon()
+           new_bonbon.text = temp
+           new_bonbon.save()
+
            return render(request, 'accountapp/hello_world.html',
-                         context={'text':' POST METHOD!'})
+                         context={'hello_world_output':new_bonbon})
     else:
            return render(request,'accountapp/hello_world.html',
                          context={'text':' POST METHOD!'})
