6e9200d7516191684f9c494a4f2de3e1fefae634
kyeongseoj <kyeongseojeon@likelion.org>
2021-07-12 16:19:05 +0900

    7/12 1ban class commit_2

--- acountapp/urls.py
+++ acountapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from acountapp.views import hello_world
+from acountapp.views import hello_world, AccountCreateView
 
 app_name = 'acountapp'
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello world')
+    path('hello_world/', hello_world, name='hello world'),
+
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
+
--- acountapp/views.py
+++ acountapp/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views import generic
+from django.views.generic import CreateView
 
 from acountapp.models import BonBon
 
@@ -21,3 +25,11 @@ def hello_world(request):
            hello_world_list = BonBon.objects.all()
            return render(request,'accountapp/hello_world.html',
                          context={'hello_world_list':hello_world_list})
+
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
