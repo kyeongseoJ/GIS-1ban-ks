dde816483852a1bdc692a06cfad7199025ee066f
kyeongseoj <kyeongseojeon@likelion.org>
2021-07-14 09:43:38 +0900

    7/14 1ban class commit_2

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,12 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-
 from accountapp.views import hello_world, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello world'),
-
+    path('hello_world/', hello_world, name='hello_world'),
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', AccountCreateView.as_view(), name='create')
 ]
 
