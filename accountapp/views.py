f4b0fe920bb462409e38af28df57838f5cce7cc0
kyeongseoj <kyeongseojeon@likelion.org>
2021-07-19 16:20:54 +0900

    7/19 1ban class commit_5

--- accountapp/views.py
+++ accountapp/views.py
@@ -13,20 +13,24 @@ from accountapp.models import BonBon
 
 
 def hello_world(request):
-    if request.method=='POST':
+    if request.user.is_authenticated:
 
-           temp = request.POST.get('hello_world_input')
+        if request.method=='POST':
 
-           new_bonbon = BonBon()
-           new_bonbon.text = temp
-           new_bonbon.save()
+               temp = request.POST.get('hello_world_input')
 
-           return HttpResponseRedirect(reverse('accountapp:hello world'))
+               new_bonbon = BonBon()
+               new_bonbon.text = temp
+               new_bonbon.save()
+
+               return HttpResponseRedirect(reverse('accountapp:hello world'))
+        else:
+               hello_world_list = BonBon.objects.all()
+               return render(request,'accountapp/hello_world.html',
+                             context={'hello_world_list':hello_world_list})
+
     else:
-           hello_world_list = BonBon.objects.all()
-           return render(request,'accountapp/hello_world.html',
-                         context={'hello_world_list':hello_world_list})
-
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 
 class AccountCreateView(CreateView):
@@ -34,8 +38,6 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
-
-
 
 class AccountDetailView(DetailView):
     model = User
