104b461d2176241d7a181e07570915ce0b0936cd
kyeongseoj <kyeongseojeon@likelion.org>
2021-07-26 15:09:13 +0900

    7/26 1ban class commit_2

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -5,5 +5,5 @@ from profileapp.views import ProfileCreateView
 app_name = 'profileapp'
 
 urlpatterns =[
-    path('create/', ProfileCreateView.as_view(), name='create' )
+    path('create/', ProfileCreateView.as_view(), name='create'),
 ]
