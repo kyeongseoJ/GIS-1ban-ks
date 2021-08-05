6c93f84d4afe15eb15a03a2ee5dece3e0de53bfd
kyeongseoj <kyeongseojeon@likelion.org>
2021-08-05 14:59:07 +0900

    08/05 1ban class commit_1.5

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -9,7 +9,6 @@ app_name ='articleapp'
 
 urlpatterns = [
     path('list/', ArticleListView.as_view(template_name='articleapp/list.html'), name='list'),
-
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
     path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
