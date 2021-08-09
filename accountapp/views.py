be08c862b8c88eed221c1ec7ea1145f74d39a409
kyeongseoj <kyeongseojeon@likelion.org>
2021-08-09 14:46:13 +0900

    08/09 1ban class commit_1

--- accountapp/views.py
+++ accountapp/views.py
@@ -59,7 +59,7 @@ class AccountUpdateView(UpdateView):
     template_name = 'accountapp/update.html'
 
     def get_success_url(self):
-        return reverse('accountapp:detail', kwargs={'pk' : self.object.pk})
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 
 @method_decorator(has_ownership, 'get')
--- profileapp/views.py
+++ profileapp/views.py
@@ -35,7 +35,7 @@ class ProfileUpdateView(UpdateView):
     template_name = 'profileapp/update.html'
 
     def get_success_url(self):
-        return reverse('accountapp:detail', kwargs= {'pk' : self.object.user.pk})
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
 
 
 
