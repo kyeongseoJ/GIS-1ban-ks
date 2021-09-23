4cad69a2b093758d47a28775f358e2e7e744aa90
kyeongseoj <kyeongseojeon@likelion.org>
2021-09-23 15:43:59 +0900

    09/23 1ban class commit_01

--- gis_1ban_0/settings/deploy.py
+++ gis_1ban_0/settings/deploy.py
@@ -1,24 +1,20 @@
 from .base import *
 
-env_list = dict()
-
-local_env = open(os.path.join(BASE_DIR,'.env'))
-
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n','')
-    start = line.find('=')
-    key = line[:start]
-    value = line[start+1:]
-    env_list[key] =value
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+# 보안 정보를 읽어오는 함수 설정
+# 경로 알기 위해서 포테이너 마리아디비 커넥트해서 확인할 수 있다. 혹은 공식문서 참고
 
+def read_secrets(secret_name):
+    file = open('/run/secrets/'+ secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
+    
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
+# .yml파일에서 가져오는 시크릿 키 사용
+SECRET_KEY =read_secrets('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -32,8 +28,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'password1234',
+        'USER': read_secrets('MARIADB_USER'),
+        'PASSWORD': read_secrets('MARIADB_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
